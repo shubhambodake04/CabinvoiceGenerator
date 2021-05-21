605ccae6ec5899171490d7e25679231e668bbf08
shubhambodake04 <shubhambodake6476@gmail.com>
2021-05-21 10:26:02 +0530

    Add project files.

---
+++ CabinvoiceGenerator/Ride.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabinvoiceGenerator
+{
+    public class Ride
+    {
+        public double distance;
+        public int time;
+        public Ride(double distance, int time)
+        {
+            this.distance = distance;
+            this.time = time;
+        }
+    }
+
+}
---
+++ CabinvoiceGenerator/RideType.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabinvoiceGenerator
+{
+  
+        public enum RideType { NORMAL, PREMIUM }
+
+}
