6cccdf76a847816084018283d35efe15ae72490d
shubhambodake04 <shubhambodake6476@gmail.com>
2021-05-30 21:20:43 +0530

    Calculate total fare for premium ride

--- CabInvoiceGeneratorTest/UnitTest1.cs
+++ CabInvoiceGeneratorTest/UnitTest1.cs
@@ -29,10 +29,28 @@ namespace CabInvoiceGeneratorTest
                 invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
                 Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 5) };
                 InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
-                InvoiceSummary expectedSummary = new InvoiceSummary(2, 35.0);
+                InvoiceSummary expectedSummary = new InvoiceSummary(2, 31.0);
                 Assert.AreEqual(expectedSummary.GetType(), summary.GetType());
             }
-
+        [Test]
+        public void GivenDistanceAndTimeForPremiumShouldReturnTotalFare()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.PREMIUM);
+            double distance = 2.0;
+            int time = 5;
+            double fare = invoiceGenerator.CalculateFare(distance, time);
+            double expected = 40;
+            Assert.AreEqual(expected, fare);
         }
+        [Test]
+        public void GivenMutipleEnhancedInvoiceShouldReturnTotalRidesTotalFareAverageFarePerRide()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(2.0, 5) };
+            InvoiceSummary enhancedSummary = invoiceGenerator.CalculateFare(rides);
+            InvoiceSummary expectedEnhancedSummary = new InvoiceSummary(2, 50);
+            Assert.AreEqual(expectedEnhancedSummary, enhancedSummary);
+        }
+    }
 
     }
